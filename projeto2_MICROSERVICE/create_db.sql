f35ae5d1b3d2ad4f69984b6d9bcfb0bba56d837f
Guilherme Alves <guilherme.alves3340@gmail.com>
2019-04-25 08:44:22 -0300

    Update db

--- projeto2_MICROSERVICE/create_db.sql
+++ projeto2_MICROSERVICE/create_db.sql
@@ -1,8 +1,9 @@
 CREATE TABLE tb_produtos(
-    id_produto INTEGER NOT NULL,
-    PRIMARY KEY(id_produto),
-    id_fornecedor INTEGER NOT NULL,
-    id_categoria INTEGER NOT NULL,
+    id_produto INTEGER NOT NULL PRIMARY KEY,
+    id_fornecedor INTEGER,
+    FOREIGN KEY (id_fornecedor) REFERENCES tb_fornecedores(id_fornecedor),
+    id_categoria INTEGER,
+    FOREIGN KEY (id_categoria) REFERENCES tb_categorias(id_categoria),
     nomeProduto VARCHAR NOT NULL,
     descricaoProduto VARCHAR NOT NULL,
     valorUnitario NUMERIC NOT NULL,
@@ -12,28 +13,29 @@ CREATE TABLE tb_produtos(
 );
 
 CREATE TABLE tb_categorias(
-    id_categoria INTEGER NOT NULL,
-    PRIMARY KEY(id_categoria),
+    id_categoria INTEGER NOT NULL PRIMARY KEY,
     tituloCategoria VARCHAR NOT NULL,
     descricaoCategoria VARCHAR NOT NULL,
     fg_ativo INTEGER NOT NULL
 );
 
 CREATE TABLE tb_vendas(
-    id_venda INTEGER NOT NULL,
-    PRIMARY KEY(id_venda),
-    id_vendedor INTEGER NOT NULL,
-    id_categoria INTEGER NOT NULL,
-    id_produto INTEGER NOT NULL,
+    id_venda INTEGER NOT NULL PRIMARY KEY,
+    id_vendedor INTEGER,
+    id_categoria INTEGER,
+    id_produto INTEGER
     dataVenda DATE NOT NULL,
     valorTotal NUMERIC NOT NULL,
     quantidade INTEGER NOT NULL,
-    fg_ativo INTEGER NOT NULL
+    fg_ativo INTEGER NOT NULL,
+
+    FOREIGN KEY (id_venda) REFERENCES tb_vendas(id_venda),
+    FOREIGN KEY (id_categoria) REFERENCES tb_categorias(id_categoria),
+    FOREIGN KEY (id_produto) REFERENCES tb_produtos(id_produto),
 );
 
 CREATE TABLE tb_fornecedores(
-    id_fornecedor INTEGER NOT NULL,
-    PRIMARY KEY(id_fornecedor),
+    id_fornecedor INTEGER NOT NULL PRIMARY KEY,
     cnpj VARCHAR NOT NULL,
     razaoSocialtelefone VARCHAR NOT NULL,
     endereco VARCHAR NOT NULL,
@@ -42,11 +44,13 @@ CREATE TABLE tb_fornecedores(
 );
 
 CREATE TABLE tb_compras(
-    id_compra INTEGER NOT NULL,
-    PRIMARY KEY(id_compra),
-    id_fornecedor INTEGER NOT NULL,
-    id_produto INTEGER NOT NULL,
-    id_categoria INTEGER NOT NULL,
+    id_compra INTEGER NOT NULL PRIMARY KEY,
+    id_fornecedor INTEGER,
+    FOREIGN KEY (id_fornecedor) REFERENCES tb_fornecedores(id_fornecedor),
+    id_produto INTEGER,
+    FOREIGN KEY (id_produto) REFERENCES tb_produtos(id_produto),
+    id_categoria INTEGER,
+    FOREIGN KEY (id_categoria) REFERENCES tb_categorias(id_categoria),
     dataCompra DATE NOT NULL,
     valorTotal NUMERIC NOT NULL,
     quantidade INTEGER NOT NULL,
@@ -54,8 +58,7 @@ CREATE TABLE tb_compras(
 );
 
 CREATE TABLE tb_vendedores(
-    id_vendedor INTEGER NOT NULL,
-    PRIMARY KEY(id_vendedor),
+    id_vendedor INTEGER NOT NULL PRIMARY KEY,
     cpf VARCHAR NOT NULL,
     nome VARCHAR NOT NULL,
     carteiraTrabalho VARCHAR NOT NULL,
---
+++ projeto2_MICROSERVICE/db.py
@@ -0,0 +1,11 @@
+
+
+
+db_postgres = {
+    'database': 'asa',
+    'password': 'postgres',
+    'schema': 'public',
+    'host': '0.0.0.0',
+    'port': 5432,
+    'username': 'postgres',
+}
