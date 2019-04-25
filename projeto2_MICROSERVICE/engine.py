f62205497f1207f252dbbe95efd0a6a6e9900b52
Guilherme Alves <guilherme.alves3340@gmail.com>
2019-04-25 14:05:13 -0300

    Update

---
+++ projeto2_MICROSERVICE/engine.py
@@ -0,0 +1,23 @@
+import logging
+
+from db import Database
+
+
+def create(data):
+    _db = Database()
+    pass
+
+
+def read(data):
+    pass
+
+
+def update(data):
+    pass
+
+
+def delete(data):
+    pass
+
+
+    
